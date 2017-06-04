789c9e2e7135cd3a7f7d93731fa0e9c48faf9d11
kramak14 <krishnanramakrishnan14@gmail.com>
2017-06-04 00:45:01 -0700

    Creates a Movie class with 4 arguments to display trailers.

---
+++ movies.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+#Creates a class Movie with 4 arguments, including self to invoke a variable.
+class Movie():
+    def __init__(self, movieTitle, movieStoryline, posterImage, trailerYouTube):
+        self.title = movieTitle
+        self.storyline = movieStoryline
+        self.poster_image_url = posterImage
+        self.trailer_youtube_url = trailerYouTube
+
+    #Shows trailer on YouTube
+    def showTrailer(self):
+        webbrowser.open(self.trailer_youtube_url)
